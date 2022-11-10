65123d5b96ab7402aaf1dc10bca74d743c043ff1
Jordan Widdison <jordan.widdison@divvypay.com>
2022-11-10 10:54:22 -0700

    Tinkering with some tmux settings

--- tmux/dev-startup.sh
+++ tmux/dev-startup.sh
@@ -9,11 +9,13 @@ if [ "$SESSIONEXISTS" = "" ]
 then
   tmux new-session -d -s "${SESSION}"
 
-  tmux rename-window -t 0 " "
-  tmux send-keys -t " " "dev" C-m "clear" C-m
+  # Make window titles be the working directory
+  tmux set-option -g status-interval 5
+  tmux set-option -g automatic-rename on
+  tmux set-option -g automatic-rename-format '#{b:pane_current_path}'
 
-  tmux new-window -t "${SESSION}":1 -n " "
-  tmux send-keys -t " " "dev" C-m "clear" C-m
+  tmux send-keys -t 0 "dev" C-m "clear" C-m
+  tmux send-keys -t "${SESSION}":1 "dev" C-m "clear" C-m
 fi
 
 tmux attach-session -t "${SESSION}":0
